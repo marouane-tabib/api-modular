5b02ba81f578d9c47e57bc2b5ee9efd3d9c9619a
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-01 10:32:31 +0100

    AddCoreBase | Make app\Repositories\BaseRepository\BaseRepository.php, and Add implementation of Repository behevior, and Add Injection of model

---
+++ app/Repositories/BaseRepository/BaseRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Repositories\BaseRepository;
+
+use App\Repositories\BaseRepository\Interfaces\Repository;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements Repository
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function get(array $select = ['*']): Collection
+    {
+        return $this->model->get($select);
+    }
+
+    public function all(array $select = ['*']): Collection
+    {
+        return $this->model->all($select);
+    }
+
+    public function paginate(int $paginate = 10): Paginator
+    {
+        return $this->model->paginate($paginate);
+    }
+
+    public function find(int $id): ?Model
+    {
+        return $this->model->find($id);
+    }
+
+    public function findOrFail(int $id): ?Model
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data): ?int
+    {
+        return $this->findOrFail($id)->update($data);
+    }
+
+    public function delete(int $id): ?bool
+    {
+        return $this->findOrFail($id)->delete();
+    }
+
+    public function with(...$with): self
+    {
+        $this->model = $this->model->with($with);
+
+        return $this;
+    }
+
+    public function where(...$where): self
+    {
+        $this->model = $this->model->where($where);
+
+        return $this;
+    }
+
+    public function select(...$select): self
+    {
+        $this->model = $this->model->select($select);
+
+        return $this;
+    }
+    
+    public function search(string $search): self
+    {
+        $this->model = $this->model->search($search);
+
+        return $this;
+    }
+
+    public function order(string $column, string $direction = 'asc'): self
+    {
+        $this->model = $this->model->orderBy($column, $direction);
+
+        return $this;
+    }
+}
