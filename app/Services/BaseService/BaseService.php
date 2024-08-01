e451907d019f3ed4d1a8fc0bde48b146e8895a2d
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-01 10:37:00 +0100

    AddCoreBase | Make app\Services\BaseService\BaseService.php file, and implementation of Service behevior, and Add injeciton of the Repository to interact with data source

---
+++ app/Services/BaseService/BaseService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services\BaseService;
+
+use App\Repositories\BaseRepository\Interfaces\Repository;
+use App\Services\BaseService\Interfaces\Service;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+
+class BaseService implements Service
+{
+    protected $repository;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index(array $data): Paginator|Model|null|Collection
+    {
+        return $this->repository
+            ->search($data['search'] ?? '')
+            ->order($data['column'] ?? 'id', $data['direction'] ?? 'asc')
+            ->get();
+    }
+
+    public function store(array $data): Model
+    {
+        return $this->repository->create($data);
+    }
+
+    public function show(int $id): ?Model
+    {
+        return $this->repository->findOrFail($id);
+    }
+
+    public function update(int $id, array $data): ?int
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function destroy(int $id): ?bool
+    {
+        return $this->repository->delete($id);
+    }
+}
