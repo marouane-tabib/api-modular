17c82ee416efe37ac1a9540658ad643e8192482c
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-01 10:55:48 +0100

    AddCoreBase | Make app\Http\Requests\Api\Concerns\HasFailedValidationResponse.php file, to customize response of the validation rule

---
+++ app/Http/Requests/Api/Concerns/HasFailedValidationResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Api\Concerns;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+trait HasFailedValidationResponse
+{
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json(
+                [
+                    "status" => 422,
+                    "success" => false,
+                    "message" => "Validation errors",
+                    "errors"  => $validator->errors(),
+                ], 422
+            )
+        );
+    }
+}
