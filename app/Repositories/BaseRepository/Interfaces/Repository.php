e56eef8e890f4ac3bf7cd4a75661a888c1be0457
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-01 10:31:16 +0100

    AddCoreBase | Make app\Repositories\BaseRepository\Interfaces\Repository.php file, with necessery methods, to define behevior of Repository

---
+++ app/Repositories/BaseRepository/Interfaces/Repository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\BaseRepository\Interfaces;
+
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface Repository
+{
+    public function all(array $select = ['*']): Collection;
+
+    public function get(array $select = ['*']): Collection;
+
+    public function paginate(int $paginate = 10): Paginator;
+
+    public function find(int $id): ?Model;
+
+    public function findOrFail(int $id): ?Model;
+
+    public function create(array $data): Model;
+
+    public function update(int $id, array $data): ?int;
+
+    public function delete(int $id): ?bool;
+
+    public function with(...$with): self;
+
+    public function where(...$where): self;
+
+    public function select(...$select): self;
+
+    public function search(string $search): self;
+
+    public function order(string $column, string $direction = 'asc'): self;
+}
