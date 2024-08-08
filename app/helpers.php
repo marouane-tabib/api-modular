ea09be1eb0f59e05b5e8974c448f4075f3826612
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-08 15:52:11 +0100

    AddCoreBase | Delete arrayMultiPluck helper function - app\helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,16 @@
 <?php
 
-function arrayMultiPluck(array $array, array $keys): array
-{
-    return array_intersect_key($array, array_flip($keys));
-}
+// function getMacAddress()
+// {
+//     $macAddress = 'N/A';
+    
+//     if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+//         $macAddress = exec('getmac');
+//     } elseif (strtoupper(substr(PHP_OS, 0, 3)) === 'DAR') {
+//         $macAddress = exec('ifconfig en0 | grep ether | awk \'{print $2}\'');
+//     } else {
+//         $macAddress = exec('cat /sys/class/net/eth0/address');
+//     }
+    
+//     return $macAddress;
+// }
