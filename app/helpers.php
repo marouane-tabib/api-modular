37b8b1f351c702a8937edb4344dc7f8b36aa1918
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-08 15:52:31 +0100

    AddCoreBase | Clean

--- app/helpers.php
+++ app/helpers.php
@@ -1,16 +1,1 @@
 <?php
-
-// function getMacAddress()
-// {
-//     $macAddress = 'N/A';
-    
-//     if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-//         $macAddress = exec('getmac');
-//     } elseif (strtoupper(substr(PHP_OS, 0, 3)) === 'DAR') {
-//         $macAddress = exec('ifconfig en0 | grep ether | awk \'{print $2}\'');
-//     } else {
-//         $macAddress = exec('cat /sys/class/net/eth0/address');
-//     }
-    
-//     return $macAddress;
-// }
