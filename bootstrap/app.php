0a0445f4e3b716669cee70999de4f0edbb31fb02
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-08 15:54:35 +0100

    AddCoreBase | Add Exception configuration, with dontReport|dontFlash|reportable|render methods, to manage the exceptions with good way  - bootstrap\app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Middleware\ForceJsonResponse;
-use App\Services\ResponderService\ResponderService;
-use Flugg\Responder\Responder;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -21,6 +20,64 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->dontReport([
+            \Illuminate\Auth\AuthenticationException::class,
+            \Illuminate\Auth\Access\AuthorizationException::class,
+            \Symfony\Component\HttpKernel\Exception\HttpException::class,
+            \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+            \Illuminate\Session\TokenMismatchException::class,
+            \Illuminate\Validation\ValidationException::class,
+        ]);
+        $exceptions->dontFlash([
+            'password',
+            'password_confirmation',
+            'current_password',
+        ]);
+        $exceptions->reportable(function (Throwable $exception) {
+            Bugsnag::notifyException($exception, function ($report) use ($exception) {
+                $request = new Request();
+                $report->setMetaData([
+                    'exception' => [
+                        'exception' => (new ReflectionClass($exception))->getShortName(),
+                        'message' => $exception->getMessage(),
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $exception->getCode(),
+                    ],
+                ]);
+                $report->setMetaData([
+                    'request' => [
+                        'url' => $request->fullUrl(),
+                        'method' => $request->method(),
+                        'headers' => $request->headers->all(),
+                        'input' => $request->all(),
+                        'query' => $request->query(),
+                        'from' => $request->query('from', 'N/A'),
+                        'to' => $request->query('to', 'N/A'),
+                        'user_agent' => $request->header('User-Agent'),
+                        'ip_address' => $request->ip(),
+                    ],
+                ]);
+                $report->setMetaData([
+                    'server' => [
+                        'php_version' => phpversion(),
+                        'software' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
+                        'environment' => app()->environment(),
+                        'hostname' => gethostname(),
+                        'server_ip' => $request->server('SERVER_ADDR', '127.0.0.1'),
+                        'port' => $request->server('SERVER_PORT', '80'),
+                        'device_name' => php_uname('n')
+                    ]
+                ]);
+                $report->setMetaData([
+                    'user' => [
+                        'id' => auth()->check() ? auth()->id() : 'N/A',
+                        'name' => auth()->check() ? auth()->user()->name : 'N/A',
+                        'email' => auth()->check() ? auth()->user()->email : 'N/A',
+                    ]
+                ]);
+            });
+        });
         $exceptions->render(function (Throwable $e, Request $request) {
             if ($request->wantsJson()) {
                 return responder()->error(message: "An unexpected error occurred. Please try again later.")->data([
