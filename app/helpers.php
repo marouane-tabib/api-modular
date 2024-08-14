03a495a8ed6a13d9a8d0bbfae1c83d9a05ac3a3e
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-14 21:35:39 +0100

    AddLoggingProcess | Make app\helpers.php file, with helpers to get git version information

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,42 @@
 <?php
+
+if (!function_exists('getReleaseVersion')) {
+    function getReleaseVersion()
+    {
+        // You can define your own way to fetch the release version, or simply return a default value.
+        return env('APP_RELEASE_VERSION', '1.0.0');
+    }
+}
+
+if (!function_exists('getCommitHash')) {
+    function getCommitHash()
+    {
+        // Execute the git command to get the latest commit hash
+        return trim(shell_exec('git log --pretty="%h" -n1 HEAD'));
+    }
+}
+
+if (!function_exists('getCommitAuthor')) {
+    function getCommitAuthor()
+    {
+        // Execute the git command to get the author of the latest commit
+        return trim(shell_exec('git log --pretty="%an" -n1 HEAD'));
+    }
+}
+
+if (!function_exists('getCommitDate')) {
+    function getCommitDate()
+    {
+        // Execute the git command to get the date of the latest commit
+        return trim(shell_exec('git log --pretty="%ci" -n1 HEAD'));
+    }
+}
+
+if (!function_exists('getAuthor')) {
+    function getAuthor()
+    {
+        // Optionally, you can define a static author or retrieve it from a specific Git configuration
+        return env('APP_AUTHOR', trim(shell_exec('git config user.name')));
+    }
+}
+
