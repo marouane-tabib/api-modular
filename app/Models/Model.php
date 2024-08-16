53d9655f5f3d530a06452401281ccd182e6507eb
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-16 14:59:31 +0100

    AddAuditProcess | Add Auditable trait with the implementation of its interface - app\Models\Model.php

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -5,8 +5,12 @@ namespace App\Models;
 use App\Models\Concerns\HasSearch;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use OwenIt\Auditing\Auditable;
+use OwenIt\Auditing\Contracts\Auditable as ContractsAuditable;
 
-class Model extends BaseModel
+class Model extends BaseModel implements ContractsAuditable
 {
-    use HasFactory, HasSearch;
+    use HasFactory;
+    use HasSearch;
+    use Auditable;
 }
