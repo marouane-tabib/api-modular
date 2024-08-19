7837bee513e3eb205102bd9344e24da1207f615d
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-19 09:08:32 +0100

    AddErrorLoggingProcess | Change the meta data value from the array to use processors method from CustomLogChannel class - app\Exceptions\GeneralExceptionHandler.php

--- app/Exceptions/GeneralExceptionHandler.php
+++ app/Exceptions/GeneralExceptionHandler.php
@@ -1,5 +1,6 @@
 <?php namespace App\Exceptions;
 
+use App\Logging\CustomLogChannel;
 use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
 use Error;
 use Exception;
@@ -38,7 +39,8 @@ class GeneralExceptionHandler
             } elseif ($exception instanceof Error) {
                 $throwableType = 'error';
             }
-            $request = request();  // Use request() helper directly
+            
+            $report->setMetaData(CustomLogChannel::processors());
             $report->setMetaData([  
                 'timestamp' => gmdate('c'),
                 'exception' => [
@@ -48,38 +50,6 @@ class GeneralExceptionHandler
                     'file' => $exception->getFile(),
                     'line' => $exception->getLine(),
                     'code' => $exception->getCode(),
-                ],
-                'request' => [
-                    'url' => $request->fullUrl(),
-                    'method' => $request->method(),
-                    'headers' => $request->except(['authorization', 'cookie', 'password', 'password_confirmation', 'current_password']),
-                    'body' => $request->except(['password', 'password_confirmation', 'current_password']),
-                    'query' => $request->query(),
-                    'from' => $request->query('from', 'N/A'),
-                    'to' => $request->query('to', 'N/A'),
-                    'user_agent' => $request->header('User-Agent'),
-                    'ip_address' => $request->ip(),
-                ],
-                'server' => [
-                    'php_version' => phpversion(),
-                    'software' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
-                    'environment' => app()->environment(),
-                    'hostname' => gethostname(),
-                    'server_ip' => $request->server('SERVER_ADDR', '127.0.0.1'),
-                    'port' => $request->server('SERVER_PORT', '80'),
-                    'device_name' => php_uname('n'),
-                ],
-                'user' => [
-                    'id' => auth()->check() ? auth()->id() : 'N/A',
-                    'name' => auth()->check() ? auth()->user()->name : 'N/A',
-                    'email' => auth()->check() ? auth()->user()->email : 'N/A',
-                ],
-                'meta_data' => [
-                    'release_version' => getReleaseVersion(),
-                    'commit_hash' => getCommitHash(),
-                    'commit_author' => getCommitAuthor(),
-                    'commit_date' => getCommitDate(),
-                    'author' => getAuthor(),
                 ],
             ]);
         });
