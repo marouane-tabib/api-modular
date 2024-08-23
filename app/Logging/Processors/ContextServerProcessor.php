ae23edc34f73dbec343f5f5499a17941d062dd07
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-24 00:36:45 +0100

    AddLoggingMonitoringProcess | Clean

--- app/Logging/Processors/ContextServerProcessor.php
+++ app/Logging/Processors/ContextServerProcessor.php
@@ -7,13 +7,13 @@ class ContextServerProcessor
     public function __invoke()
     {
         return [
-                'php_version' => phpversion(),
-                'software' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
-                'environment' => app()->environment(),
-                'hostname' => gethostname(),
-                'server_ip' => request()->server('SERVER_ADDR', '127.0.0.1'),
-                'port' => request()->server('SERVER_PORT', '80'),
-                'device_name' => php_uname('n')
+            'php_version' => phpversion(),
+            'software' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
+            'environment' => app()->environment(),
+            'hostname' => gethostname(),
+            'server_ip' => request()->server('SERVER_ADDR', '127.0.0.1'),
+            'port' => request()->server('SERVER_PORT', '80'),
+            'device_name' => php_uname('n')
         ];
     }
 }
