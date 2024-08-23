1523ef93c7984b68792f634208a0b3f493be20ee
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-23 12:23:51 +0100

    AddLoggingMonitoringProcess | Change the processors method return from the static values, to use processers - app\Logging\CustomLogChannel.php

--- app/Logging/CustomLogChannel.php
+++ app/Logging/CustomLogChannel.php
@@ -2,6 +2,7 @@
 
 namespace App\Logging;
 
+use App\Logging\Processors;
 use Illuminate\Support\Facades\File;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -63,41 +64,10 @@ class CustomLogChannel
     {
         return [
             'timestamp' => gmdate('c'),
-            'request' => [
-                'url' => request()->fullUrl(),
-                'method' => request()->method(),
-                'headers' => request()->except(['authorization', 'cookie', 'password', 'password_confirmation', 'current_password']),
-                'body' => request()->except(['password', 'password_confirmation', 'current_password']),
-                'query' => request()->query(),
-                'from' => request()->query('from', 'N/A'),
-                'to' => request()->query('to', 'N/A'),
-                'user_agent' => request()->header('User-Agent'),
-                'ip_address' => request()->ip(),
-            ],
-            
-            'server' => [
-                'php_version' => phpversion(),
-                'software' => $_SERVER['SERVER_SOFTWARE'] ?? 'N/A',
-                'environment' => app()->environment(),
-                'hostname' => gethostname(),
-                'server_ip' => request()->server('SERVER_ADDR', '127.0.0.1'),
-                'port' => request()->server('SERVER_PORT', '80'),
-                'device_name' => php_uname('n')
-            ],
-
-            'user' => [
-                'id' => auth()->check() ? auth()->id() : 'N/A',
-                'name' => auth()->check() ? auth()->user()->name : 'N/A',
-                'email' => auth()->check() ? auth()->user()->email : 'N/A',
-            ],
-            
-            'meta_data' => [
-                'release_version' => getReleaseVersion(),
-                'commit_hash' => getCommitHash(),
-                'commit_author' => getCommitAuthor(),
-                'commit_date' => getCommitDate(),
-                'author' => getAuthor(),
-            ]
+            'request' => (new Processors\ContextRequestProcessor())(),
+            'server' => (new Processors\ContextServerProcessor())(),
+            'user' => (new Processors\ContextUserProcessor())(),
+            'meta_data' => (new Processors\ContextVersionProcessor())()
         ];
     }
 }
