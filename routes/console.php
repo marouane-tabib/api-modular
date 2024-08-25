497e215286da0c32703de853c63b1d8015fd1e75
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-25 12:20:02 +0100

    AddLoggingMonitoringProcess | Add Schedule command to run it datily, to send http & queries logs to elastic channels - routes\console.php

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('logs:send-to-elasticsearch')->daily();
+Schedule::command('logs:info-to-channel http_requests elastic_info_http')->daily();
+Schedule::command('logs:info-to-channel data_base_queries elastic_info_queries')->daily();
