6d5c45bef41b878c24c5f6c0ca63ddf37fbc3afd
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-25 12:12:04 +0100

    AddLoggingMonitoringProcess | Make app\Console\Commands\Log\FileInfoLogsToChannel.php file, to get the info logs files form public puth

---
+++ app/Console/Commands/Log/FileInfoLogsToChannel.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Console\Commands\Log;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class FileInfoLogsToChannel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:send-to-elasticsearch {infoDirectory} {channel="elasticsearch"}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send daily logs to Elasticsearch';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $date = Carbon::yesterday();
+
+        $infoDirectory = $this->argument('infoDirectory');
+        $logChannel = $this->argument('channel');
+        $severityLevel = 'info';
+        $message = 'Info daily logs';
+
+        $logFilePath = storage_path('logs/info/' . $date->format('Y-m') . '/'. $infoDirectory .'/' . $date->format('Y-m-d') . '.log');
+
+        if (!file_exists($logFilePath)) {
+            $this->error("Log file not found: $logFilePath");
+            return 1;
+        }
+
+        Artisan::call('logs:file-log-to-channel', [
+            'file_path' => $logFilePath,
+            'channel' => $logChannel,
+            'severity_level' => $severityLevel,
+            '--message' => $message,
+        ]);
+
+        $this->info("Logs from $logFilePath have been sent to Elasticsearch successfully.");
+        
+        return 0;
+    }
+}
