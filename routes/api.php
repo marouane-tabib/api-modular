5c81a24f2aec7415d4b51199a5be3db8df47a2bf
marouane-tabib <tabibmarouane6@gmail.com>
2024-08-29 12:18:36 +0100

    AddAuthProcess | Add auth necessery routes, and Add auth:api meddleware in the users route - routes\api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Controller;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/users', Controller::class)->middleware('auth:api');
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
