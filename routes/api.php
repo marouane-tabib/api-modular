f911c39b4944e407228c856522dd78b575eb3906
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-01 15:30:35 +0100

    AddModularRestApiProcess | Add throttle:user_throttle with middleware condition - routes\api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\Controller;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/users', Controller::class)->middleware('role:manager');
+Route::apiResource('/users', Controller::class)->middleware('throttle:user_throttle');
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
