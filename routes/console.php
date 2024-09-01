12463b1d731586b1c4db5bd9a5bfa2d1de25efb1
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-01 01:16:52 +0100

    AddModularRestApiProcess | Delete not necessery commands - routes\console.php

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command('logs:info-to-channel http_requests elastic_info_http')->daily();
 Schedule::command('logs:info-to-channel data_base_queries elastic_info_queries')->daily();
