6a50caad8c279c4ce4e1ceab94a2f96ae622c68d
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-01 16:47:35 +0100

    AddModularRestApiProcess | Make app\Http\Middleware\ForceSSL.php file, to checke the request secure or not

---
+++ app/Http/Middleware/ForceSSL.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ForceSSL
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->secure() && in_array(config('app.env'), ['stage', 'production'])) {
+            throw new HttpException(403, 'SSL Certificate is required for this environment. Please ensure your connection is secure.');
+        }
+
+        return $next($request);
+    }
+}
