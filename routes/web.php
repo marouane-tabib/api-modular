b9f735735612d1c3b828868d0e6f8bbee5c5903a
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-01 17:07:29 +0100

    AddModularRestApiProcess | Clean

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::middleware([
 //     'auth:sanctum',
