ef849b119b86d8c3d6e9b9b8bae21edb238b87c4
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-02 14:56:19 +0100

    AddModularRestApiProcess | Make database\migrations\9999_99_99_000000_add_deleted_at_to_specific_tables.php file, with condition to add softDelete in all tables of database

---
+++ database/migrations/9999_99_99_000000_add_deleted_at_to_specific_tables.php
@@ -0,0 +1,38 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    protected $tables = [];
+
+    public function up()
+    {
+        $this->tables = array_map('current', DB::select('SHOW TABLES'));
+
+        foreach ($this->tables as $tableName) {
+            if ($tableName === 'migrations') {
+                continue;
+            }
+
+            if (!Schema::hasColumn($tableName, 'deleted_at')) {
+                Schema::table($tableName, function (Blueprint $table) {
+                    $table->softDeletes();
+                });
+            }
+        }
+    }
+
+    public function down()
+    {
+        foreach ($this->tables as $tableName) {
+            if (Schema::hasColumn($tableName, 'deleted_at')) {
+                Schema::table($tableName, function (Blueprint $table) {
+                    $table->dropSoftDeletes();
+                });
+            }
+        }
+    }
+};
