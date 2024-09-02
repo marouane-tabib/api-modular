c44e70e46a0203b4c7a8ee8e5dbd2ebbd811473c
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-02 14:26:07 +0100

    AddModularRestApiProcess | Add backup:run with --only-db option, as a schedule command every day - routes\console.php

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('logs:info-to-channel http_requests elastic_info_http')->daily();
 Schedule::command('logs:info-to-channel data_base_queries elastic_info_queries')->daily();
+Schedule::command('backup:run --only-db')->daily();
