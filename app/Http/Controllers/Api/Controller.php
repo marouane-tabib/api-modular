30cd46eb4d296acc4993232a6d5e5485a71f778a
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-03 16:24:30 +0100

    AddUserModuleProcess | Add mehtod return types with ResponseBuilder class, and Change the syntax of the attributes - app\Http\Controllers\Api\Controller.php

--- app/Http/Controllers/Api/Controller.php
+++ app/Http/Controllers/Api/Controller.php
@@ -5,37 +5,42 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller as BaseController;
 use App\Http\Requests\Api\Request as ApiRequest;
 use App\Services\BaseService\Interfaces\Service;
+use Flugg\Responder\Http\Responses\ResponseBuilder;
 use Flugg\Responder\Responder;
 
 class Controller extends BaseController
 {
+    protected $service, $request, $responder;
+
     public function __construct(
-        protected Service $service,
-        protected Responder $responder
+        Service $service, ApiRequest $request, Responder $responder
     ) {
+        $this->service = $service;
+        $this->request = $request;
+        $this->responder = $responder;
     }
 
-    public function index(ApiRequest $request)
+    public function index(ApiRequest $request): ResponseBuilder
     {
         return $this->responder->success($this->service->index($request->validated()));
     }
 
-    public function show($id)
+    public function show($id): ResponseBuilder
     {
         return $this->responder->success($this->service->show($id));
     }
 
-    public function store(ApiRequest $request)
+    public function store(ApiRequest $request): ResponseBuilder
     {
-        return $this->responder->success($this->service->store($request->all()));
+        return $this->responder->success($this->service->store($request->validated()));
     }
 
-    public function update(ApiRequest $request, $id)
+    public function update(ApiRequest $request, $id): ResponseBuilder
     {
         return $this->responder->success($this->service->update($id, $request->validated()));
     }
 
-    public function destroy($id)
+    public function destroy($id): ResponseBuilder
     {
         return $this->responder->success($this->service->destroy($id));
     }
