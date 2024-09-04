4d920b459ae4526b0804baa64601b7ae45522217
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-04 14:50:15 +0100

    RefactoreMakeModuleProcess | Make app\Console\Commands\Module\ServiceMakeCommand.php file, to overreading getTemplateContents

---
+++ app/Console/Commands/Module/ServiceMakeCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands\Module;
+
+use Nwidart\Modules\Commands\Make\ServiceMakeCommand as MakeServiceMakeCommand;
+use Nwidart\Modules\Support\Stub;
+
+class ServiceMakeCommand extends MakeServiceMakeCommand
+{
+    protected function getTemplateContents(): string
+    {
+        $module = $this->laravel['modules']->findOrFail($this->getModuleName());
+
+        return (new Stub($this->getStubName(), [
+            'STUDLY_NAME' => $module->getStudlyName(),
+            'CLASS_NAMESPACE' => $this->getClassNamespace($module),
+            'CLASS' => $this->getClassNameWithoutNamespace(),
+        ]))->render();
+    }
+    
+    private function getClassNameWithoutNamespace(): array|string
+    {
+        return class_basename($this->getServiceName());
+    }
+}
