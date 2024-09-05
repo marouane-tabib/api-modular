1cc06bbaf849399c2fea98566e24bf7807a93300
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-05 15:41:18 +0100

    AddAuthModuleProcess | Delete registration of api and web routes - bootstrap\app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,6 @@ use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
