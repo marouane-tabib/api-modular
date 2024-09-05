943edb670b604813ef563b9d35d44cd0986be7f0
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-05 14:56:58 +0100

    AddAuthModuleProcess | Make Modules\Auth\routes derictory, with the api_v1 route file, to register the auth endpoints

---
+++ Modules/Auth/routes/api_v1.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Auth\Http\Controllers\AuthController;
+
+/*
+ *--------------------------------------------------------------------------
+ * API Routes
+ *--------------------------------------------------------------------------
+ *
+ * Here is where you can register API routes for your application. These
+ * routes are loaded by the RouteServiceProvider within a group which
+ * is assigned the "api" middleware group. Enjoy building your API!
+ *
+*/
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('refresh', [AuthController::class, 'refresh']);
