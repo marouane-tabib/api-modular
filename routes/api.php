952e888e6b1772c7a3ce12100fc032863c4d7b87
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-05 14:59:13 +0100

    AddAuthModuleProcess | Delete routes web | api

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('refresh', [AuthController::class, 'refresh']);
