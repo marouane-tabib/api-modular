0cc6addeb9c16c0296a72756ada80e55ad099c9f
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-11 21:31:50 +0100

    AddAuthModuleProcess | Change the Trait to HasFailedValidationResponse, to change the response from session to response - app\Http\Requests\Request.php

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Requests;
 
-use App\Http\Requests\Concerns\HasFailedValidationResponseMessages;
+use App\Http\Requests\Api\Concerns\HasFailedValidationResponse;
 use Illuminate\Foundation\Http\FormRequest;
 
 class Request extends FormRequest
 {
-    use HasFailedValidationResponseMessages;
-
+    use HasFailedValidationResponse;
     /**
      * Determine if the user is authorized to make this request.
      */
