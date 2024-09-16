d195c059482252faaf0c9bbfe402314fd7e0559e
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-16 13:51:41 +0100

    base | Make app\Http\Middleware\SecurityHeaders.php file, to set the values in the header, for more security

---
+++ app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        
+        $response->headers->set('X-Frame-Options', 'DENY');
+        $response->headers->set('X-Content-Type-Options', 'nosniff');
+        $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->headers->set('Content-Security-Policy', "default-src 'self';");
+        $response->headers->set('Referrer-Policy', 'no-referrer');
+
+        return $response;
+    }
+}
