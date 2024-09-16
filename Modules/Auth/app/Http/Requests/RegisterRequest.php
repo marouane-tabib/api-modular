80f938beba940ba56af67ed568d8dac9bacf62af
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-16 13:50:40 +0100

    AddStrongPasswordValidator | Add strong_password validation rule - Modules\Auth\app\Http\Requests\RegisterRequest.php

--- Modules/Auth/app/Http/Requests/RegisterRequest.php
+++ Modules/Auth/app/Http/Requests/RegisterRequest.php
@@ -14,7 +14,7 @@ class RegisterRequest extends Request
         return [
             'name'                  => 'required|string|min:4|max:50',
             'email'                 => 'required|email|min:5|max:100|unique:users,email',
-            'password'              => 'required|string|min:8|max:100|confirmed',
+            'password'              => 'required|string|min:8|max:100|strong_password|confirmed',
             'password_confirmation' => 'required',
         ];
     }
