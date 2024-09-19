4cec6e4e939ee9792278baff9c65cf6b0286f338
marouane-tabib <tabibmarouane6@gmail.com>
2024-09-19 16:22:15 +0100

    base | Change users model to use it in the auth - config\auth.php

--- config/auth.php
+++ config/auth.php
@@ -66,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model'  => env('AUTH_MODEL', App\Models\User::class),
+            'model'  => env('AUTH_MODEL', Modules\User\Models\User::class),
         ],
 
         // 'users' => [
